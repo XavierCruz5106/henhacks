6087e55b2a5c3edd3b4c9c35db6d3c55987bdd84
XavierCruz5106 <superguyman121@gmail.com>
2025-03-01 13:23:30 -0500

    feat: adds stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,22 @@
 import "@/app/globals.css"
-import { Mona_Sans as FontSans } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+import { Roboto as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+  weight: "100"
 })
 
-export default function RootLayout({ children }) {
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   )
 }
-
